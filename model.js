811c0ce8a230b6bc91a90f5572dbc375c615550f
christis96 <christian.ulstein@gmail.com>
2025-04-03 14:45:49 +0200

    todo list
    added TODO lists

--- model.js
+++ model.js
@@ -10,7 +10,7 @@ const model = {
         csPerMin: ""
     },
     data: {
-        lastGamesSummoners:{
+        lastGamesSummoners:{            //TODO: gjør det mulig for denne delen å ta inn array fra getavgstats funksjonen
             avgWinrate: "",
             avgKDA: "",
 
